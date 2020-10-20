613de2bab9cc9368ce5b4b9e4e465b12e7d4b2f5
ichillous <isiah.chillous@gmail.com>
2020-10-20 15:23:29 -0400

    Updated GET request for Tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,22 @@
 class TagsController < ApplicationController
+    def new
+        @tag = Tag.new
+    end
+    # 1 tag <-- belongs to --> 1 post
+    def show
+        @tag = Tag.find(params[:name])
+        render json: @tags
+    end
+
+    #GET /tags
+    def index
+        @tags = Tag.all
+        render json: @tags
+    end
     
+    def create
+        tag = Tag.new(params[:tag].permit(:name, :category))
+        @tag.save
+        redirect_to @tag
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   
   resources :posts do 
     resources :comments
+    
   end
+  resources :tags
+
 end
