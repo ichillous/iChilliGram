399699f13e3f4a356dd346dec328d2d96f3f89d7
ichillous <isiah.chillous@gmail.com>
2020-10-20 13:03:13 -0400

    Updated Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    def index
+        @post = Post.find(params[:post_id])
+        render json: @post.comments
+    end
+
+    def create
+        @comment = Comment.new(comment_params) 
+        @post = Post.find(params[:post_id])
+        @comment.save 
+        @post.comments << @comment
+        render json: @post 
+    end
+    #DELETE /comments/:id
+    def destroy
+        @comment.destroy
+    end
+
+    private
+    def comment_params 
+        params.require(:comment).permit(:username, :content)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_025351) do
+ActiveRecord::Schema.define(version: 2020_10_20_161958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_10_20_025351) do
     t.boolean "like"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_10_20_025351) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
